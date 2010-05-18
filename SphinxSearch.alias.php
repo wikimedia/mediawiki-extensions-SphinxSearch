5ceff36c28a97ceccb4518db9798454ebb9878e4
Raimond Spekking <raymond@users.mediawiki.org>
2010-05-18 20:00:55 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-05-18 19:45:00 UTC)

--- SphinxSearch.alias.php
+++ SphinxSearch.alias.php
@@ -14,18 +14,23 @@ $aliases['en'] = array(
 	'SphinxSearch' => array( 'SphinxSearch' ),
 );
 
+/** Arabic (العربية) */
 $aliases['ar'] = array(
-	'SphinxSearch'   => array( 'بحث_سفنكس' ),
+	'SphinxSearch' => array( 'بحث_سفنكس' ),
 );
 
+/** Japanese (日本語) */
 $aliases['ja'] = array(
-	'SphinxSearch'   => array( 'Sphinx検索' ),
+	'SphinxSearch' => array( 'Sphinx検索' ),
 );
 
+/** Luxembourgish (Lëtzebuergesch) */
 $aliases['lb'] = array(
-	'SphinxSearch'   => array( 'Sphinx Sich' ),
+	'SphinxSearch' => array( 'Sphinx Sich' ),
 );
 
+/** Dutch (Nederlands) */
 $aliases['nl'] = array(
-	'SphinxSearch'   => array( 'SphinxZoeken' ),
+	'SphinxSearch' => array( 'SphinxZoeken' ),
 );
+
